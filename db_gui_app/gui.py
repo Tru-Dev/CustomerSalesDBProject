689254e6b37d28c7153c0eb09644527394ec35a1
Tru-Dev <29785998+Tru-Dev@users.noreply.github.com>
2021-09-02 10:17:41 -0700

    Added Welcome Screen (closes #3)

--- db_gui_app/gui.py
+++ db_gui_app/gui.py
@@ -29,11 +29,31 @@ class Welcome(tk.Frame):
     def __init__(self, host: UIHost, master: Optional[tk.Tk]=None):
         super().__init__(master)
         self.master = master
+        self.host = host
         self.master.title(config.config.windowtitle.format("Welcome"))
         self.pack(fill=tk.BOTH, expand=True)
-        # self.create_widgets()
+        self.create_widgets()
 
-    # def create_widgets(self):
-        # self.
-
-
+    def create_widgets(self):
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
+        self.columnconfigure(2, weight=1)
+        self.rowconfigure(0, weight=2)
+        self.rowconfigure(1, weight=4)
+        self.rowconfigure(2, weight=3)
+        self.main_frame = tk.Frame(self)
+        self.main_frame.grid(column=1, row=1, sticky="N")
+        self.main_label = tk.Label(
+            self.main_frame, text="Customer Sales DB", font=("Segoe UI", 15), pady=10
+        )
+        self.main_label.grid(column=0, row=0)
+        self.customers_btn = tk.Button(self.main_frame, text="Customers")
+        self.customers_btn.grid(column=0, row=1, sticky="NEW")
+        self.products_btn = tk.Button(self.main_frame, text="Products")
+        self.products_btn.grid(column=0, row=2, sticky="NEW")
+        self.sales_btn = tk.Button(self.main_frame, text="Sales")
+        self.sales_btn.grid(column=0, row=3, sticky="NEW")
+        self.exit_btn = tk.Button(
+            self.main_frame, text="Exit", fg="white", bg="red", command=self.master.destroy
+        )
+        self.exit_btn.grid(column=0, row=4, sticky="NEW")
